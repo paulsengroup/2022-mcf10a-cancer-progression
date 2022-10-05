6f114ecc6bbdd68b243dcf73c7172302c28b5fcd
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-10-05 18:23:11 +0200

    Add run_nfcore_hic scripts

---
+++ run_nfcore_hic.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2022 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -u
+set -o pipefail
+
+function run_workflow() {
+  set -e
+  set -u
+  set -o pipefail
+
+  fq="$1"
+
+  bname="${fq%_R1.fastq.gz}"
+  sample_name="$(basename "$bname")"
+  outdir="$(readlink -f ./data/scratch/nfcore_hic)"
+  input_name="${bname}{_R1,_R2}.fastq.gz"
+
+  echo 2>&1 "Processing $bname..."
+
+  if [[ $HOSTNAME == *.saga* ]]; then
+    sbatch -A "${SLURM_PROJECT_ID-changeme}" \
+      -c 2 --mem 3G -t 4-00:00:00 \
+      --job-name="nfcore_hic_$sample_name" \
+      --wait \
+      "scripts/run_nfcore_hic_workflow.sh" \
+      "$(readlink -f .)" \
+      "$outdir" \
+      "$sample_name" \
+      "$input_name" \
+      --process.cache=deep \
+      --max_cpus 52 \
+      --max_memory '400.GB' \
+      --max_time 336.h &>/dev/null
+  else
+    scripts/run_nfcore_hic_workflow.sh \
+      "$(readlink -f .)" \
+      "$outdir" \
+      "$sample_name" \
+      "$input_name" \
+      --process.cache=deep \
+      --max_cpus 64 \
+      --max_memory '500.GB' \
+      --max_time 336.h &>/dev/null
+  fi
+
+  echo 2>&1 "DONE processing $bname!"
+}
+
+export -f run_workflow
+
+if [[ $HOSTNAME == *.saga* ]]; then
+  parallel_runs=999
+else
+  parallel_runs=4
+fi
+
+mkdir -p ./data/scratch/nfcore_hic
+raw_reads_dir="$(readlink -f ./data/input/raw_data/hic)"
+# Note: It is important that files are named like *_R[12].fastq.gz.
+# Furthermore, file names shall not contain single digit fields:
+# For example, mysample_001_R1.fastq.gz is ok, but mysample_1_R1.fastq.gz is not ok!
+printf "%s\n" "$raw_reads_dir/"*R1.fastq.gz |
+  xargs -n 1 -P "$parallel_runs" -I {} bash -c 'run_workflow "$@"' _ {}
---
+++ scripts/run_nfcore_hic_workflow.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2022 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -o pipefail
+set -u
+
+argc="$#"
+
+if [ $argc -lt 4 ]; then
+  echo "Usage: $0 input_dir output_dir sample_name input_pattern"
+  echo "Example: $0 2022-david-hic/ 2022-david-hic/data/scratch/nfcore-hic 10A HiC_001_10A{_R1,_R2}.fastq.gz"
+  exit 1
+fi
+
+indir="$1"
+outdir="$2"
+sample_name="$3"
+input_name="$4"
+args=("${@:5}")
+data_dir="$indir/data"
+
+wd="$NXF_WORK/nfcore_hic_$sample_name"
+launch_dir="$indir/.launch-dir/$sample_name"
+mkdir -p "$wd" "$outdir/$sample_name" "$launch_dir"
+
+scratch_dir="$(mktemp -d "$launch_dir/scratch-XXXXXX")"
+trap "rm -rf '$scratch_dir'" EXIT
+
+echo 2>&1 "Extracting bowtie2 index..." &&
+  zstd -dcf "$data_dir/output/preprocessing/bowtie2_idx/GRCh38_genome_assembly.tar.zst" |
+  tar -xf - -C "$scratch_dir/" &&
+  echo 2>&1 "DONE"
+
+bw2_idx_files=("$scratch_dir"*.b2)
+
+bw2_idx="${bw2_idx_files[0]}"
+
+rm -rf "$launch_dir/hic"
+git clone --depth=1 --branch 20220513 https://github.com/robomics/hic.git "$launch_dir/hic"
+ln -sf "$(readlink -f ./containers)/" "$launch_dir/containers"
+ln -sf "$(readlink -f ./data)/" "$launch_dir/data"
+
+# I am not sure who's creating these symlinks
+sleep 5 && (unlink ./data/data || true) && (unlink ./containers/containers || true) &
+
+# Note: It is important that files are named like *_R[12].fastq.gz.
+# Furthermore, file names shall not contain single digit fields:
+# For example, mysample_001_R1.fastq.gz is ok, but mysample_1_R1.fastq.gz is not ok!
+cd "$launch_dir"
+nextflow run ./hic \
+  "${args[@]}" \
+  --input "$input_name" \
+  --outdir "$outdir/$sample_name" \
+  --bwt2_index "$bw2_idx" \
+  -e."TMPDIR=$TMPDIR" \
+  -c "$indir/configs/base_saga.config" \
+  -c "$indir/configs/nfcore_hic.config" \
+  -ansi-log false \
+  -resume |&
+  tee "$outdir/$sample_name/nfcore_hic.log"
