a48465a94cd3e04e45cf95b1ee0f3a2d5e15c159
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2022-10-20 15:08:32 +0200

    Add script to pull containers used by the workflow

---
+++ pull_singularity_containers.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2022 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -o pipefail
+set -u
+set -x
+
+mkdir -p containers/cache
+
+read -r -d '' -a uris < <(find configs -type f -exec grep "docker://.*[\"']" {} + |
+                          sed -E "s|.*(docker://.*)[\"']|\1|" |
+                          sort -u && printf '\0')
+
+echo "uris: ${uris[*]}"
+
+for uri in "${uris[@]}"; do
+    name="$(echo "$uri" | sed -E 's|docker://(.*)|\1|' | tr  -c '[:alnum:].\n' '-').img"
+    singularity pull --disable-cache -F --name "containers/cache/$name" "$uri" &> /dev/null \
+    && echo "Done processing $uri..." &
+done
+
+echo "Waiting for pulls to complete..."
+wait
+echo "DONE!"
