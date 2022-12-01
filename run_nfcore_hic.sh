paulsengroup/2022-mcf10a-cancer-progression

commit 1fbee96ec5cc353189b2e4df4e63abd4703fe42b

Update nfcore/hic runner scripts and config

run_nfcore_hic.sh
@@ -17,7 +17,7 @@ function run_workflow() {
 
   bname="${fq%_R1.fastq.gz}"
   sample_name="$(basename "$bname")"
-  outdir="$(readlink -f ./data/scratch/nfcore_hic)"
+  outdir="$(readlink -f ./data/output/nfcore_hic)"
   input_name="${bname}{_R1,_R2}.fastq.gz"
 
   printf 2>&1 "Processing %s...\n" "$sample_name"

configs/nfcore_hic.config
@@ -16,7 +16,7 @@ params {
     skip_tads = true
     skip_compartments = true
     skip_mcool = true
-    skip_multiqc = false
+    skip_multiqc = true
 
     split_fastq = true
     fastq_chunks_size = 15000000

scripts/run_nfcore_hic_workflow.sh
@@ -12,7 +12,7 @@ argc="$#"
 
 if [ $argc -lt 4 ]; then
   echo "Usage: $0 input_dir output_dir sample_name input_pattern"
-  echo "Example: $0 2022-david-hic/ 2022-david-hic/data/scratch/nfcore-hic 10A HiC_001_10A{_R1,_R2}.fastq.gz --max_cpus=50 --max_memory=500.GB --max_time=300.h"
+  echo "Example: $0 2022-mcf10a-cancer-progression/ 2022-mcf10a-cancer-progression/data/output/nfcore-hic 10A HiC_001_10A{_R1,_R2}.fastq.gz --max_cpus=50 --max_memory=500.GB --max_time=300.h"
   exit 1
 fi
 
@@ -24,7 +24,7 @@ args=("${@:5}")
 data_dir="$indir/data"
 
 wd="$NXF_WORK/nfcore_hic_$sample_name"
-launch_dir="$indir/.launch-dir/$sample_name"
+launch_dir="$indir/.nfcore-hic-wd/$sample_name"
 sentinel="$launch_dir/done"
 
 mkdir -p "$wd" "$outdir/$sample_name" "$launch_dir"
@@ -34,11 +34,16 @@ if [ -f "$sentinel" ]; then
   exit 0
 fi
 
+# Sleep for up to 60s
+sleep $((RANDOM % 60))
+
 scratch_dir="$(mktemp -d "$launch_dir/scratch-XXXXXX")"
 
 function cleanup {
   exit_code=$?
-  if [ $exit_code -ne 0 ]; then
+  if [ $exit_code -eq 0 ]; then
+    nextflow clean -f
+  else
     rm -f "$sentinel"
   fi
 
@@ -83,5 +88,3 @@ nextflow run ./hic \
 
 echo 2>&1 "Touching \"$sentinel\"..."
 touch "$sentinel"
-
-nextflow clean -f $(nextflow log -q | tail -n 1)

commit db504197441d2b2fdc4363001619aa9593e13f38

Fix bug introduced with 1fbee96

run_nfcore_hic.sh
@@ -17,7 +17,7 @@ function run_workflow() {
 
   bname="${fq%_R1.fastq.gz}"
   sample_name="$(basename "$bname")"
-  outdir="$(readlink -f ./data/output/nfcore_hic)"
+  outdir="$(readlink -f ./data/scratch/nfcore_hic)"
   input_name="${bname}{_R1,_R2}.fastq.gz"
 
   printf 2>&1 "Processing %s...\n" "$sample_name"