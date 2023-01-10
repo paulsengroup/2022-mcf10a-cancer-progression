760d5b6981346a0a53432cf411dcd62d7b131b9c
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-10 14:39:40 +0100

    Add skeleton for Chrom3D workflow

---
+++ run_chrom3d_workflow.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+#
+# SPDX-License-Identifier: MIT
+
+set -e
+set -o pipefail
+set -u
+
+echo 1>&2 'Running robomics/call_tad_cliques...'
+
+wd=".nextflow-chrom3d-wd"
+mkdir -p "$wd"
+
+for dir in configs containers data scripts workflows; do
+  (cd "$wd" && ln -sf "../$dir/" "$dir")
+done
+
+if [[ $HOSTNAME == *.saga* ]]; then
+    args=("${@:2}"
+        --max_memory=400.G
+        --max_cpus=52
+        --max_time=336.h
+        --project="${SLURM_PROJECT_ID-changeme}")
+else
+    args=()
+fi
+
+./remove_symlink_loops.sh
+(cd "$wd" &&
+nextflow run https://github.com/robomics/call_tad_cliques \
+  -r v0.0.8 \
+  "${args[@]}" \
+  -c configs/call_tad_cliques_chrom3d.config \
+  -resume
+)
+
+exit 0  # TODO: removeme!
+
+function run_workflow() {
+    name="$1"
+    if [[ $HOSTNAME == *.saga* ]]; then
+        base_config='configs/base_saga.config'
+        args=("${@:2}"
+            --max_memory=400.G
+            --max_cpus=52
+            --max_time=336.h
+            --project="${SLURM_PROJECT_ID-changeme}")
+    else
+        base_config='configs/base_hovig.config'
+        args=("${@:2}")
+    fi
+
+    for dir in configs containers data scripts workflows; do
+        ln -sf "../$dir/" "$dir"
+    done
+
+    ../remove_symlink_loops.sh
+    nextflow run \
+        "${args[@]}" \
+        -c "configs/$name.config" \
+        -c "$base_config" \
+        -process.cache=deep \
+        "workflows/$name.nf" \
+        -ansi-log \
+        -resume
+}
+
+step='chrom3d'
+echo 1>&2 "Running step $step..."
+
+wd=".nextflow-$step-wd"
+mkdir -p "$wd"
+(cd "$wd" && run_workflow "$step")
+echo 1>&2 "Workflow \"$step\" successfully completed!"
---
+++ workflows/chrom3d.nf
@@ -0,0 +1,10 @@
+#!/usr/bin/env nextflow
+// Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
+//
+// SPDX-License-Identifier: MIT
+
+nextflow.enable.dsl=2
+
+workflow {
+    // placeholder
+}
