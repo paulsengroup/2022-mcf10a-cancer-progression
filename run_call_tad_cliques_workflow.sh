61641f69b6d4d9ff748fa09ce33faa77cba04501
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-01-15 16:30:09 +0100

    Update call_tad_cliques configs and runner script

--- run_call_tad_cliques_workflow.sh
+++ run_call_tad_cliques_workflow.sh
@@ -30,7 +30,7 @@ fi
 ./remove_symlink_loops.sh
 (cd "$wd" &&
 nextflow run https://github.com/robomics/call_tad_cliques \
-  -r v0.0.8 \
+  -r v0.0.10 \
   "${args[@]}" \
   -c configs/call_tad_cliques.config \
   -resume
@@ -39,7 +39,7 @@ nextflow run https://github.com/robomics/call_tad_cliques \
 ./remove_symlink_loops.sh
 (cd "$wd" &&
 nextflow run https://github.com/robomics/call_tad_cliques \
-  -r v0.0.8 \
+  -r v0.0.10 \
   "${args[@]}" \
   -c configs/call_tad_cliques_vs_control.config \
   -resume
