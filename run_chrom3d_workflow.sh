2aeafc15f34c1dd308de04b4b53c8dfa2a159764
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-03-01 19:56:25 +0100

    Refactor runner scripts

--- run_chrom3d_workflow.sh
+++ run_chrom3d_workflow.sh
@@ -43,35 +43,5 @@ nextflow run https://github.com/robomics/call_tad_cliques \
 
 exit 0  # TODO: removeme!
 
-function run_workflow() {
-    name="$1"
-    if [[ $HOSTNAME == *.saga* ]]; then
-        base_config='configs/base_saga.config'
-        args=("${@:2}"
-            --max_memory=400.G
-            --max_cpus=52
-            --max_time=336.h
-            --project="${SLURM_PROJECT_ID-changeme}")
-    else
-        base_config='configs/base_hovig.config'
-        args=("${@:2}")
-    fi
-
-    for dir in bin configs containers data workflows; do
-        ln -sf "../$dir/" "$dir"
-    done
-
-    ../remove_symlink_loops.sh
-    nextflow run \
-        "${args[@]+"${args[@]}"}" \
-        -c "configs/$name.config" \
-        -c "$base_config" \
-        -process.cache=deep \
-        "workflows/$name.nf" \
-        -ansi-log \
-        -resume
-}
-
-echo 1>&2 "Running step $step..."
-(cd "$wd" && run_workflow "$step")
-echo 1>&2 "Workflow \"$step\" successfully completed!"
+./setup_workflow_workdir.sh "$PWD" "$wd"
+./run_workflow.sh "$wd" "$step"
