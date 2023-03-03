b292df478df086b3f48e4764fadd2f79bc2c19ff
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-03-03 23:10:12 +0100

    Run black and isort on new python scripts

--- bin/make_diploid_gtrack.py
+++ bin/make_diploid_gtrack.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
-import sys
 import re
+import sys
 
 gtrack_fname = str(sys.argv[1])
 
 with open(gtrack_fname) as gf:
-  gtrack_file = gf.readlines()
+    gtrack_file = gf.readlines()
 
 for line in gtrack_file:
-  line=line.rstrip()
-  if line.startswith("#"):
-    print(line)
-  else:
-    line1=re.sub(r'(chr\w+)',r'\1_A',line)
-    line2=re.sub(r'(chr\w+)',r'\1_B',line)
-    print(line1)
-    print(line2)
+    line = line.rstrip()
+    if line.startswith("#"):
+        print(line)
+    else:
+        line1 = re.sub(r"(chr\w+)", r"\1_A", line)
+        line2 = re.sub(r"(chr\w+)", r"\1_B", line)
+        print(line1)
+        print(line2)
