03310c0d4c84e9e83fc24ec43d2ebada97f045ff
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-04-22 20:51:21 +0200

    Update RNA-seq workflows (#63)
    - Remove unused scripts and Dockerfile
    - Update script to run DESeq2
    - Update `diff_expression_analysis` workflow

--- run_nfcore_rnaseq.sh
+++ run_nfcore_rnaseq.sh
@@ -8,7 +8,6 @@ set -e
 set -o pipefail
 set -u
 
-
 wd=".nextflow-nfcore-rnaseq-wd"
 mkdir -p "$wd"
 
@@ -18,16 +17,4 @@ mkdir -p "$wd"
 ./run_external_workflow.sh \
   "$wd" \
   'workflows/nfcore-rnaseq-v3.11.1.tar.xz' \
-   configs/nfcore/nfcore_rnaseq*.json
-
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP1_WT MCF10A_WT_REP1 --replace
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP2_WT MCF10A_WT_REP2 --replace
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP3_WT MCF10A_WT_REP3 --replace
-
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP1_T1 MCF10A_T1_REP1 --replace
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP2_T1 MCF10A_T1_REP2 --replace
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP3_T1 MCF10A_T1_REP3 --replace
-
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP1_C1 MCF10A_C1_REP1 --replace
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP2_C1 MCF10A_C1_REP2 --replace
-bin/fix_nfcore_rnaseq_output.sh "$wd/data/output/nfcore_rnaseq" MCF10A_REP3_C1 MCF10A_C1_REP3 --replace
+  configs/nfcore/nfcore_rnaseq*.json
