b7fdea7345fbc09f800e3cafa2ea73952cf9381d
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-04-23 19:01:32 +0200

    Update subcompartment clustering process and scripts

--- bin/cluster_domains_by_subcompartment_state.py
+++ bin/cluster_domains_by_subcompartment_state.py
@@ -8,24 +8,15 @@ import argparse
 import functools
 import logging
 import pathlib
+import pickle
 import sys
-from typing import Dict, List, Tuple
+from typing import List, Tuple, Union
 
 import hdbscan
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
-import seaborn as sns
 
 
 def make_cli():
-    def positive_int(arg):
-        if (n := int(arg)) > 0:
-            return n
-
-        raise ValueError("Not a positive integer")
-
     def count_or_fraction(arg) -> float:
         n = float(arg)
         if n >= 1 and n.is_integer():
@@ -34,6 +25,9 @@ def make_cli():
             return n
 
         raise ValueError("Not a count or fraction")
+
+    def comma_separated_string(s: str) -> Tuple[str]:
+        return tuple(s.split(","))
 
     cli = argparse.ArgumentParser()
 
@@ -44,9 +38,14 @@ def make_cli():
         help="TSV or BED file produced by annotate_domains_with_subcompartments.py.",
     )
     cli.add_argument(
+        "--labels",
+        type=comma_separated_string,
+        help="Comma-separated list of labels used to label conditions. Should be in the same order as the input TSV(s).",
+    )
+    cli.add_argument(
         "--min-cluster-size",
         type=count_or_fraction,
-        default=25,
+        default=0.01,
         help="Minimum cluster size.\n"
         "Values in range [0, 1) are interpreted as fractions of the total number of datapoints.\n"
         "See HDBSCAN* documentation for more details.",
@@ -69,7 +68,7 @@ def make_cli():
     cli.add_argument(
         "--cluster-selection-epsilon",
         type=float,
-        default=0.1,
+        default=0.05,
         help="See HDBSCAN* documentation for more details.",
     )
     cli.add_argument(
@@ -84,19 +83,8 @@ def make_cli():
         "--output-prefix",
         type=pathlib.Path,
         required=True,
-        help="Path to output prefix (including parent folder(s) but without extension).",
+        help="Path to use as output prefix (including parent folder(s) but without extension).",
     )
-    cli.add_argument(
-        "--no-plotting",
-        default=False,
-        action="store_true",
-        help="Skip plot generation. Only output a TSV with the clustered data.",
-    )
-
-    cli.add_argument("--plot-title", type=str, default="", help="Title to use for plotting.")
-
-    cli.add_argument("--cmap", type=str, default="deep", help="Colormap used for plotting.")
-
     cli.add_argument(
         "--force",
         action="store_true",
@@ -119,118 +107,21 @@ def get_compartment_ranks() -> dict:
     return {k: v for v, k in enumerate(compartment_labels)}
 
 
-def import_tsv(path_to_tsvs: List[pathlib.Path]) -> pd.DataFrame:
+def import_tsv(path_to_tsvs: List[pathlib.Path], labels: Union[List[str], None]) -> pd.DataFrame:
+    assert labels is None or len(path_to_tsvs) == len(labels)
     stdin = {pathlib.Path("stdin"): sys.stdin, pathlib.Path("-"): sys.stdin}
-    return pd.concat((pd.read_table(stdin.get(path, path)) for path in path_to_tsvs))
 
+    paths = ((stdin.get(path, path)) for path in path_to_tsvs)
+    if labels is None:
+        labels = [p.name.partition(".")[0] for p in path_to_tsvs]
 
-@functools.cache
-def get_color_palette_dict(
-    num_colors: int, cmap: str = "deep", outlier_color=(0.5, 0.5, 0.5)
-) -> Dict[int, Tuple[float, float, float]]:
-    palette = {i: c for i, c in enumerate(sns.color_palette(cmap, n_colors=num_colors))}
-    palette[-1] = outlier_color
-    return palette
+    dfs = []
+    for label, path in zip(labels, paths):
+        df = pd.read_table(path)
+        df["label"] = label
+        dfs.append(df)
 
-
-@functools.cache
-def get_color_palette_list(
-    num_colors: int, cmap: str = "deep", outlier_color=(0.5, 0.5, 0.5)
-) -> List[Tuple[float, float, float]]:
-    return [
-        color
-        for _, color in sorted(
-            get_color_palette_dict(num_colors, cmap, outlier_color).items(), key=lambda item: item[0]
-        )
-    ]
-
-
-def get_cluster_colors(clusters: pd.Series, pvalues: pd.Series, palette: Dict) -> List:
-    base_colors = [palette[i] for i in clusters]
-
-    assert len(clusters) == len(pvalues)
-    return [sns.desaturate(x, p) for x, p in zip(base_colors, pvalues)]
-
-
-def plot_scatter(df: pd.DataFrame, suptitle: str, cmap="deep") -> plt.Figure:
-    cols = df.filter(regex=r"[AB\d]\.state").columns.tolist()
-
-    color_palette = get_color_palette_dict(num_colors=df["cluster"].max() + 1, cmap=cmap)
-    cluster_colors = get_cluster_colors(df["cluster"], df["cluster_pval"], color_palette)
-
-    fig, axs2d = plt.subplots(len(cols), len(cols), figsize=(6.4 * 4, 6.4 * 4))
-    for i, (col1, axs1d) in enumerate(zip(cols, axs2d)):
-        for j, (col2, ax) in enumerate(zip(cols, axs1d)):
-            if j >= i:
-                ax.scatter(df[col1], df[col2], alpha=0.2, s=3, c=cluster_colors)
-                ax.set(xlim=(-0.1, 1.1), ylim=(-0.1, 1.1))
-
-    legend_patches = [mpl.patches.Patch(facecolor=c, label=f"Cluster #{i}") for i, c in color_palette.items() if i >= 0]
-    legend_patches.append(mpl.patches.Patch(facecolor=color_palette[-1], label="Outliers"))
-
-    fig.legend(handles=legend_patches, loc="lower left")
-    fig.suptitle(suptitle)
-    plt.tight_layout()
-    return fig
-
-
-def rank_clusters(df: pd.DataFrame) -> List[int]:
-    """
-    Rank clusters based on mean subcompartment rank.
-    This can be useful to aid comparison across different plots
-    """
-    ranked_clusters = {}
-    for cluster_id, modal_states in df.groupby("cluster")["state.mode"]:
-        ranked_clusters[cluster_id] = modal_states.map(get_subcompartment_ranks()).mean()
-
-    # Rank clusters by value (i.e. by avg state rank)
-    ranked_clusters = [int(k) for k, _ in sorted(ranked_clusters.items(), key=lambda item: item[1])]
-
-    # Override order for outlier cluster
-    ranked_clusters.remove(-1)
-    ranked_clusters.insert(0, -1)
-
-    return ranked_clusters
-
-
-def plot_distribution(df: pd.DataFrame, suptitle: str, cmap="deep") -> plt.Figure:
-    cols = df.filter(regex=r"[AB\d]\.state").columns.tolist()
-
-    num_clusters = df["cluster"].max() + 1
-    color_palette = get_color_palette_list(num_colors=num_clusters, cmap=cmap)
-
-    fig, axs = plt.subplots(num_clusters, 1, figsize=(20, 2 * num_clusters))
-    for cluster_id, ax, color in zip(rank_clusters(df), axs, color_palette):
-        cluster_df = df.loc[df["cluster"] == cluster_id, cols]
-        cluster_size = len(cluster_df)
-        cluster_size_rel = cluster_size / len(df)
-        for _, row in cluster_df.iterrows():
-            ax.plot(row, color=color, alpha=0.1)
-
-        if cluster_id >= 0:
-            title = f"Cluster #{cluster_id} ({cluster_size} obs; ({100 * cluster_size_rel:.2f}%))"
-        else:
-            title = f"Outliers ({len(cluster_df)} obs; ({100 * cluster_size_rel:.2f}%))"
-        ax.set(title=title, xlim=(0, 1), ylim=(0, 1))
-
-        ax.set_xticks(range(len(cols)))
-        ax.set_xticklabels((c.removesuffix(".state") for c in cols))
-
-    fig.suptitle(suptitle)
-    plt.tight_layout()
-    return fig
-
-
-def plot_outlier_scores(df: pd.DataFrame, suptitle: str) -> plt.Figure:
-    fig, ax = plt.subplots(1, 1)
-
-    scores = df.loc[np.isfinite(df["outlier_score"]), "outlier_score"]
-    sns.histplot(scores, kde=True, ax=ax)
-    sns.rugplot(scores, ax=ax)
-
-    fig.suptitle(suptitle)
-    plt.tight_layout()
-    return fig
+    return pd.concat(dfs)
 
 
 def run_clustering(
@@ -256,18 +147,6 @@ def run_clustering(
     df[cols] = m
 
     return df, clusterer
-
-
-def save_plot_to_file(fig: plt.Figure, outprefix: pathlib.Path, force: bool, close_after_save: bool = True) -> None:
-    png = outprefix.with_suffix(".png")
-    svg = outprefix.with_suffix(".svg")
-    if not force:
-        handle_path_collisions(png, svg)
-
-    fig.savefig(png, bbox_inches="tight", dpi=300)
-    fig.savefig(svg, bbox_inches="tight")
-    if close_after_save:
-        plt.close(fig)
 
 
 def compute_min_cluster_size(df: pd.DataFrame, n: float) -> int:
@@ -306,7 +185,7 @@ def main():
 
     out_prefix.parent.mkdir(parents=True, exist_ok=True)
 
-    df = import_tsv(args["tsv"])
+    df = import_tsv(args["tsv"], args.get("labels"))
 
     min_cluster_size = compute_min_cluster_size(df, args["min_cluster_size"])
     min_samples = compute_min_samples(df, args["min_samples"])
@@ -320,30 +199,18 @@ def main():
         cluster_selection_epsilon=cluster_selection_epsilon,
     )
 
-    outname = pathlib.Path(f"{out_prefix}_clusters.tsv.gz")
+    outname = out_prefix.with_suffix(".clusters.tsv.gz")
     if not args["force"]:
         handle_path_collisions(outname)
 
     df.to_csv(outname, sep="\t", index=False, header=True)
 
-    print(df["cluster"].unique())
+    outname = out_prefix.with_suffix(".clusterer.pickle.xz")
+    if not args["force"]:
+        handle_path_collisions(outname)
 
-    if not args["no_plotting"]:
-        suptitle = "" if args["plot_title"] == "" else args["plot_title"] + " - "
-        suptitle += (
-            f"HDBSCAN* (min_cluster_size={min_cluster_size}; "
-            f"min_samples={min_samples}; "
-            f"cluster_selection_method={cluster_selection_method})"
-        )
-
-        fig = plot_distribution(df, suptitle=suptitle, cmap=args["cmap"])
-        save_plot_to_file(fig, pathlib.Path(f"{out_prefix}_lineplot"), args["force"])
-
-        fig = plot_outlier_scores(df, suptitle=suptitle)
-        save_plot_to_file(fig, pathlib.Path(f"{out_prefix}_outliers"), args["force"])
-
-        fig = plot_scatter(df, suptitle=suptitle, cmap=args["cmap"])
-        save_plot_to_file(fig, pathlib.Path(f"{out_prefix}_scatter"), args["force"])
+    with open(outname, "wb") as f:
+        pickle.dump(clusterer, f, pickle.HIGHEST_PROTOCOL)
 
 
 if __name__ == "__main__":
--- workflows/comparative_analysis.nf
+++ workflows/comparative_analysis.nf
@@ -41,11 +41,37 @@ workflow {
         file(params.cliques, checkIfExists: true)
     )
 
+    annotate_domains_with_subcompartments.out.tads
+        .groupTuple()
+        .map { tuple(it[0], 'tad', it.drop(1).flatten()) }
+        .set { annotated_tads }
+
+    annotate_domains_with_subcompartments.out.cliques
+        .groupTuple()
+        .map { tuple(it[0], 'clique', it.drop(1).flatten()) }
+        .set { annotated_cliques }
+
     cluster_domains_by_subcompartment(
-        annotate_domains_with_subcompartments.out.tads.groupTuple()
-            .join(annotate_domains_with_subcompartments.out.cliques.groupTuple())
-            .map { tuple(it[0], it.drop(1).flatten().sort()) }
+        annotated_tads.mix(annotated_cliques),
+        params.condition_labels.join(','),
+        params.hdbscan_min_cluster_size,
+        params.hdbscan_min_samples,
+        params.hdbscan_cluster_selection_method,
+        params.hdbscan_epsilon,
+        params.hdbscan_dist
     )
+
+    cluster_domains_by_subcompartment.out.pickle
+        .join(cluster_domains_by_subcompartment.out.tsv,
+              by: [0, 1],
+              failOnDuplicate: true,
+              failOnMismatch: true)
+        .combine(params.condition_labels + ['all'])
+        .set { plot_domain_clusters_inputs }
+        // [resolution, label, clusterer, cluster, condition]
+
+
+    plot_domain_clusters(plot_domain_clusters_inputs)
 
     overlap_subcomps_with_expression_lvls(
         subcomp_bgs,
@@ -135,45 +161,86 @@ process annotate_domains_with_subcompartments {
 }
 
 process cluster_domains_by_subcompartment {
-    publishDir "${params.output_dir}/annotated_domains/", mode: 'copy'
+    publishDir "${params.output_dir}/annotated_domains/", mode: 'copy',
+                                                          saveAs: { "${resolution}/clusters/${it}" }
     label 'process_short'
 
     input:
         tuple val(resolution),
+              val(label),
               path(annotated_domains)
 
+        val labels
+        val min_size
+        val min_samples
+        val selection_method
+        val epsilon
+        val metric
+
     output:
-        tuple val(resolution), path("${resolution}/*.tsv.gz"), emit: clusters
-        tuple val(resolution), path("${resolution}/plots/*.png"), emit: png
-        tuple val(resolution), path("${resolution}/plots/*.svg"), emit: svg
+        tuple val(resolution),
+              val(label),
+              path("*.pickle.*"),
+        emit: pickle
+        tuple val(resolution),
+              val(label),
+              path("*.tsv.gz"),
+        emit: tsv
 
     shell:
-        outprefix=annotated_domains.collect { "${resolution}/${it.simpleName}" }.join(" ")
+        outprefix = label
         '''
-        inputs=(!{annotated_domains})
-        outputs=(!{outprefix})
-
-        for i in "${!inputs[@]}"; do
-            cluster_domains_by_subcompartment_state.py \\
-                "${inputs[$i]}" \\
-                --output-prefix="${outputs[$i]}"
-        done
-
         cluster_domains_by_subcompartment_state.py \\
-            *_cliques.tsv.gz \\
-            --output-prefix='!{resolution}/cliques_merged'
+            --labels '!{labels}' \\
+            --min-cluster-size '!{min_size}' \\
+            --min-samples '!{min_samples}' \\
+            --cluster-selection-method '!{selection_method}' \\
+            --cluster-selection-epsilon '!{epsilon}' \\
+            --distance-metric '!{metric}' \\
+            *MCF10A_{WT,T1,C1}*.gz \\
+            --output-prefix='!{outprefix}'
+        '''
+}
 
-        cluster_domains_by_subcompartment_state.py \\
-            *_domains.bed.gz \\
-            --output-prefix='!{resolution}/tads_merged'
+process plot_domain_clusters {
+    publishDir "${params.output_dir}/annotated_domains/", mode: 'copy',
+                                                          saveAs: { "${resolution}/clusters/plots/${it}" }
+    label 'process_short'
 
-        mkdir '!{resolution}/plots'
-        mv '!{resolution}/'*.{png,svg} '!{resolution}/plots/'
+    input:
+        tuple val(resolution),
+              val(label),
+              path(clusterer),
+              path(clusters),
+              val(condition_of_interest)
+
+    output:
+        tuple val(resolution),
+              val(label),
+              val(condition_of_interest),
+              path("*.png"),
+        emit: png
+        tuple val(resolution),
+              val(label),
+              val(condition_of_interest),
+              path("*.svg"),
+        emit: svg
+
+    shell:
+        '''
+        plot_domain_subcompartment_clusters.py \\
+            '!{clusters}' \\
+            '!{clusterer}' \\
+            --plot-type=line \\
+            --plot-title='!{label} ('!{condition_of_interest}')' \\
+            --label-to-highlight='!{condition_of_interest}' \\
+            --output-prefix='!{label}_!{condition_of_interest}'
         '''
 }
 
 process overlap_subcomps_with_expression_lvls {
     publishDir "${params.output_dir}/subcomps_vs_rnaseq/plots/", mode: 'copy'
+    label 'process_short'
 
     input:
         tuple val(resolution),
@@ -196,5 +263,6 @@ process overlap_subcomps_with_expression_lvls {
             '!{resolution}/subcomps_vs_expression' \\
             --labels='!{labels}' \\
             --sample-name-mappings-tsv=<(tail -n +2 '!{sample_name_mappings}')
+            # tail -n +2 is used to skip the header
         '''
 }
