0957cb5dbfec521d8756d89fcee493adc1e96b5c
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2023-07-04 19:20:14 +0200

    Update tad_analysis workflow

--- bin/generate_insulation_report.py
+++ bin/generate_insulation_report.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2022 Roberto Rossini <roberros@uio.no>
+
+# Copyright (C) 2023 Roberto Rossini <roberros@uio.no>
 #
 # SPDX-License-Identifier: MIT
 
 import argparse
 import pathlib
+import sys
 import warnings
-from math import inf
-from typing import List
+from typing import Dict, Tuple
 
 import bioframe as bf
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
@@ -19,24 +21,23 @@ def make_cli():
     cli = argparse.ArgumentParser()
 
     cli.add_argument(
-        "bedgraph",
-        nargs="+",
+        "insulation",
         type=pathlib.Path,
-        help="Path to one or more bedgraph file(s) with the insulation scores to be processed.",
+        help="Path to the aggregated insulation score bedgraph.",
     )
 
     cli.add_argument(
-        "-o",
-        "--output-prefix",
+        "domains",
         type=pathlib.Path,
-        required=True,
-        help="Path to output prefix (including parent folder(s) but without extension).",
+        help="Path to a BED file with the list of TADs to be used in the report (usually WT).",
     )
 
     cli.add_argument(
-        "--labels",
-        type=str,
-        help="Comma separated list of sample labels to use instead of those inferred from file names.",
+        "--blacklist", type=pathlib.Path, help="Path to a BED file with the list of regions to be excluded."
+    )
+
+    cli.add_argument(
+        "--blacklist-padding", type=int, default=100_000, help="Number of bps to add as padding to blacklisted regions."
     )
 
     cli.add_argument(
@@ -55,13 +56,14 @@ def import_bedgraph(path_to_bedgraph: pathlib.Path) -> pd.DataFrame:
         return bf.read_table(path_to_bedgraph, schema="bedGraph").set_index(["chrom", "start", "end"])
 
 
-def import_insulation_scores(paths: List[pathlib.Path], labels: List[str]) -> pd.DataFrame:
-    assert len(paths) == len(labels)
-    dfs = []
-    for label, path in zip(labels, paths):
-        dfs.append(import_bedgraph(path).rename(columns={"value": label}))
+def import_insulation_scores(path: pathlib.Path) -> pd.DataFrame:
+    return pd.read_table(path).set_index(["chrom", "start", "end"])
 
-    return pd.concat(dfs, axis="columns", join="outer")
+
+def import_tads(path: pathlib.Path) -> pd.DataFrame:
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        return bf.read_table(path, schema="bed3").set_index(["chrom", "start", "end"])
 
 
 def detect_path_collisions(*args: pathlib.Path) -> None:
@@ -70,45 +72,67 @@ def detect_path_collisions(*args: pathlib.Path) -> None:
             raise RuntimeError(f"Refusing to overwrite file {path}. Pass --force to overwrite existing file(s).")
 
 
-def main():
-    args = vars(make_cli().parse_args())
+def compute_insulation_std(scores: pd.DataFrame, mappings: Dict) -> pd.DataFrame:
+    stds = []
+    for _, row in scores.iterrows():
+        std = 0.0
+        for columns in mappings.values():
+            std += row.loc[columns].std(skipna=False)
+        stds.append(std)
 
-    output_prefix = args["output_prefix"]
-    if not args["force"]:
-        detect_path_collisions(output_prefix.with_suffix(".png"), output_prefix.with_suffix(".svg"))
+    scores["std"] = stds
+    return scores.sort_values("std", ascending=False)
 
-    labels = args.get("labels")
-    paths_to_scores = args["bedgraph"]
-    if labels is None:
-        labels = [p.name for p in paths_to_scores]
-    else:
-        labels = labels.split(",")
-        if len(labels) != len(paths_to_scores):
-            raise RuntimeError(
-                f"Mismatch in the number of files and labels: expected {len(paths_to_scores)} labels, found {len(labels)}"
-            )
 
-    scores = import_insulation_scores(paths_to_scores, labels)
+def map_insulation_to_tads(domains: pd.DataFrame, scores: pd.DataFrame) -> pd.DataFrame:
+    df1 = domains.reset_index().copy()
 
-    grid_size = len(scores.columns)
+    df1["end"] = df1["start"] + 1
 
-    fig, axs = plt.subplots(grid_size, grid_size, figsize=(grid_size * 6.4, grid_size * 6.4))
+    df1 = (
+        bf.overlap(df1, scores.reset_index(), suffixes=("", "_"))
+        .drop(columns=["chrom_", "start_", "end_"])
+        .rename(columns=lambda c: c.rstrip("_"))
+    )
 
-    lb = inf
-    ub = -inf
-    for i in range(grid_size):
-        for j in range(i + 1, grid_size):
+    df2 = (
+        bf.overlap(domains.reset_index(), df1, suffixes=("", "_"))
+        .drop(columns=["chrom_", "start_", "end_"])
+        .rename(columns=lambda c: c.rstrip("_"))
+        .set_index(["chrom", "start", "end"])
+    )
+    df2["std"] = df2.std(axis="columns")
+
+    return df2
+
+
+def compute_axis_scale(scores: pd.DataFrame) -> Tuple[float, float]:
+    lb = np.inf
+    ub = -np.inf
+    num_cols = len(scores.columns)
+    for i in range(num_cols):
+        for j in range(i + 1, num_cols):
             cond1, cond2 = scores.columns[i], scores.columns[j]
             df = scores[[cond1, cond2]].dropna()
-            axs[i][j].scatter(df[cond1], df[cond2])
-
             lb = min(lb, df[cond1].min(), df[cond2].min())
             ub = max(ub, df[cond1].max(), df[cond2].max())
 
     ub += abs(ub) * 0.05
     lb -= abs(lb) * 0.05
-    for i in range(grid_size):
-        for j in range(grid_size):
+
+    return lb, ub
+
+
+def plot_scatters(fig, axs, lb, ub, scores: pd.DataFrame):
+    num_cols = len(scores.columns)
+    for i in range(num_cols):
+        for j in range(i + 1, num_cols):
+            cond1, cond2 = scores.columns[i], scores.columns[j]
+            df = scores.copy()
+            df["delta"] = (df[cond1] - df[cond2]).abs()
+            axs[i][j].scatter(df[cond1], df[cond2], color="blue", alpha=0.1)
+    for i in range(num_cols):
+        for j in range(num_cols):
             ax = axs[i][j]
 
             if i == 0:
@@ -122,10 +146,37 @@ def main():
             else:
                 ax.set(xlim=(lb, ub), ylim=(lb, ub))
 
-    plt.tight_layout()
 
-    fig.savefig(output_prefix.with_suffix(".png"), dpi=600)
-    fig.savefig(output_prefix.with_suffix(".svg"))
+def main():
+    args = vars(make_cli().parse_args())
+
+    paths_to_scores = args["insulation"]
+    paths_to_domains = args["domains"]
+
+    scores = import_insulation_scores(paths_to_scores)
+    domains = import_tads(paths_to_domains)
+
+    if args["blacklist"] is not None:
+        df1 = domains.copy()
+
+        for col in df1.index.names:
+            df1[col] = df1.index.get_level_values(col)
+
+        df1 = bf.expand(df1, args["blacklist_padding"])
+        domains = bf.setdiff(df1, bf.read_table(args["blacklist"], schema="bed")).drop(
+            columns=["chrom", "start", "end"]
+        )
+
+    scores = map_insulation_to_tads(domains, scores)
+    scores.to_csv(sys.stdout, sep="\t", index=True, header=True, na_rep="NaN")
+
+    grid_size = len(scores.columns) - 1
+    fig, axs = plt.subplots(grid_size, grid_size, figsize=(grid_size * 6.4, grid_size * 6.4))
+    lb, ub = compute_axis_scale(scores)
+    plot_scatters(fig, axs, lb, ub, scores.drop(columns=["std"]))
+
+    plt.tight_layout()
+    fig.savefig("/tmp/test.scatter.png")
 
 
 if __name__ == "__main__":
--- workflows/tad_analysis.nf
+++ workflows/tad_analysis.nf
@@ -30,11 +30,57 @@ workflow {
                          .groupTuple(by: [1, 2])
                          .set { tads }
 
-    generate_tad_report(tads,
-                        params.repl_pretty_labels,
-                        params.cond_pretty_labels)
+    generate_chrom_sizes(
+        mcools.first(),
+        resolutions.first()
+    )
+
+    insulation_to_bigwig(
+        hicexplorer_find_tads.out.tads,
+        generate_chrom_sizes.out.chrom_sizes
+    )
+
+    aggregate_insulation_scores(
+        tads,
+        params.repl_labels,
+        params.cond_labels
+    )
+
+    generate_tad_report(
+        tads,
+        params.repl_pretty_labels,
+        params.cond_pretty_labels
+    )
+
+    generate_insulation_report(
+        aggregate_insulation_scores.out.bedgraph,
+        tads.map { it[5] }.collect(), // domains
+        file(params.blacklist),
+        params.repl_pretty_labels,
+        params.cond_pretty_labels
+    )
 }
 
+process generate_chrom_sizes {
+
+    input:
+        tuple val(sample_type), path(cooler)
+        val resolution
+
+    output:
+        path "*.chrom.sizes", emit: chrom_sizes
+
+    shell:
+        outprefix="${cooler.baseName}"
+        '''
+        if [ '!{resolution}' -ne 0 ]; then
+            cooler='!{cooler}::/resolutions/!{resolution}'
+        else
+            cooler='!{cooler}'
+        fi
+        cooler dump "$cooler" -t chroms > '!{outprefix}.chrom.sizes'
+        '''
+}
 
 process apply_normalization_to_coolers {
     input:
@@ -59,10 +105,10 @@ process apply_normalization_to_coolers {
             cooler='!{cooler}'
         fi
 
-        cooler_apply_normalization.py              \
-                    --norm-name='!{normalization}' \
-                    --output-name='!{out}'         \
-                    --nproc='!{task.cpus}'         \
+        cooler_apply_normalization.py              \\
+                    --norm-name='!{normalization}' \\
+                    --output-name='!{out}'         \\
+                    --nproc='!{task.cpus}'         \\
                     "$cooler"
         '''
 }
@@ -97,9 +143,9 @@ process hicexplorer_find_tads {
         NUMEXPR_MAX_THREADS='!{task.cpus}'
         export NUMEXPR_MAX_THREADS
 
-        hicFindTADs -p '!{task.cpus}'          \
-                    --matrix '!{cooler}'       \
-                    --outPrefix '!{outprefix}' \
+        hicFindTADs -p '!{task.cpus}'          \\
+                    --matrix '!{cooler}'       \\
+                    --outPrefix '!{outprefix}' \\
                     --correctForMultipleTesting fdr
 
         # Compress text files
@@ -107,6 +153,78 @@ process hicexplorer_find_tads {
             xargs -L 1 -P '!{task.cpus}' sh -c 'gzip -9 "$1"' sh
         '''
 }
+
+process insulation_to_bigwig {
+    publishDir "${params.output_dir}", mode: 'copy',
+                                       saveAs: {
+                                             it = it.toString().replaceAll("_${normalization}_${resolution}_", "_")
+                                            "${normalization}/${resolution}/${it}"
+                                       }
+
+    label 'process_very_short'
+
+    input:
+        tuple val(type),
+              val(normalization),
+              val(resolution),
+              path(boundaries),
+              path(domains),
+              path(insulation_score),
+              path(tad_score)
+        path chrom_sizes
+
+    output:
+        path "*.bw", emit: bw
+
+    shell:
+        outprefix="${insulation_score.simpleName}"
+        '''
+        zcat '!{insulation_score}' |
+        sort -k1,1 -k2,2n > scores.bedgraph
+
+        bedGraphToBigWig scores.bedgraph '!{chrom_sizes}' '!{outprefix}.bw'
+        '''
+}
+
+process aggregate_insulation_scores {
+    publishDir "${params.output_dir}", mode: 'copy',
+                                       saveAs: { "${normalization}/${resolution}/${it}" }
+
+    label 'process_short'
+
+    input:
+        tuple val(type),
+              val(normalization),
+              val(resolution),
+              path(boundaries),
+              path(domains),
+              path(insulation_score),
+              path(tad_score)
+
+        val labels_replicates
+        val labels_conditions
+
+    output:
+        tuple val(type),
+              val(normalization),
+              val(resolution),
+              path("*.bedgraph.gz"), emit: bedgraph
+
+    shell:
+        suffix="_${normalization}_${resolution}"
+        '''
+        aggregate_insulation_scores.py \\
+            *WT*merged!{suffix}_domains.bed.gz \\
+            --labels='!{labels_replicates}' |
+             gzip -9 > 'insulation_scores_replicates.bedgraph.gz'
+
+        aggregate_insulation_scores.py \\
+            *WT*merged!{suffix}_domains.bed.gz \\
+            --labels='!{labels_conditions}' |
+             gzip -9 > 'insulation_scores_conditions.bedgraph.gz'
+        '''
+}
+
 
 process generate_tad_report {
     publishDir "${params.output_dir}", mode: 'copy',
@@ -120,7 +238,7 @@ process generate_tad_report {
               val(resolution),
               path(boundaries),
               path(domains),
-              path(scores),
+              path(insulation_scores),
               path(tad_score)
 
         val labels_replicates
@@ -144,15 +262,49 @@ process generate_tad_report {
             --output-prefix=report_conditions \
             --labels='!{labels_conditions}'
 
-        generate_insulation_report.py \
-            hg38_???_*!{suffix}_score.bedgraph.gz \
-            --output-prefix=report_replicates_insulation \
-            --labels='!{labels_replicates}'
+        mkdir plots
+        mv *.svg *.png plots/
+        '''
+}
 
-        generate_insulation_report.py \
-            *{WT,T1,C1}_merged!{suffix}_score.bedgraph.gz \
-            --output-prefix=report_conditions_insulation \
-            --labels='!{labels_conditions}'
+process generate_insulation_report {
+    publishDir "${params.output_dir}", mode: 'copy',
+                                       saveAs: { "${normalization}/${resolution}/${it}" }
+
+    label 'process_short'
+
+    input:
+        tuple val(type),
+              val(normalization),
+              val(resolution),
+              path(insulation)
+
+        path tads
+        path blacklist
+
+        val labels_replicates
+        val labels_conditions
+
+    output:
+        path "plots/*.png", emit: png
+        path "plots/*.svg", emit: svg
+        path "*.tsv", emit: tsv
+
+    shell:
+        suffix="_${normalization}_${resolution}"
+        '''
+        if [[ '!{type}' == cond ]]; then
+            labels='!{labels_conditions}'
+        else
+            labels='!{labels_replicates}'
+        fi
+
+        generate_insulation_report.py \\
+            '!{insulation}' \\
+            *WT_*merged!{suffix}_domains.bed.gz \
+            --output-prefix=report_replicates_insulation \\
+            --blacklist '!{blacklist}' \\
+            --labels="$labels"
 
         mkdir plots
         mv *.svg *.png plots/
