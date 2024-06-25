257e2f7c126faaf470ca2c0bf97b48c60b49b503
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-06-25 12:09:11 +0200

    Update call_tad_cliques workflow

--- run_call_tad_cliques_workflow.sh
+++ run_call_tad_cliques_workflow.sh
@@ -17,7 +17,7 @@ mkdir -p "$wd"
 1>&2 echo 'Running robomics/call_tad_cliques...'
 ./run_external_workflow.sh \
   "$wd" \
-  'workflows/robomics-call_tad_cliques-v0.4.1.tar.xz' \
+  'workflows/robomics-call_tad_cliques-v0.4.2.tar.xz' \
    configs/call_tad_cliques_merged.json
 
 wd=".nextflow-robomics-call-tad-cliques-wd2"
@@ -28,7 +28,7 @@ mkdir -p "$wd"
 1>&2 echo 'Running robomics/call_tad_cliques...'
 ./run_external_workflow.sh \
   "$wd" \
-  'workflows/robomics-call_tad_cliques-v0.4.1.tar.xz' \
+  'workflows/robomics-call_tad_cliques-v0.4.2.tar.xz' \
    configs/call_tad_cliques_repl.json
 
 
