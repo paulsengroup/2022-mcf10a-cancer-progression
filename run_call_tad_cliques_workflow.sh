bb0053f871d86814fb1c0ad165d97d586609eaec
Roberto Rossini <71787608+robomics@users.noreply.github.com>
2024-06-25 14:42:16 +0200

    Update cliques workflow

--- run_call_tad_cliques_workflow.sh
+++ run_call_tad_cliques_workflow.sh
@@ -32,6 +32,29 @@ mkdir -p "$wd"
    configs/call_tad_cliques_repl.json
 
 
+wd=".nextflow-robomics-call-tad-cliques-wd3"
+mkdir -p "$wd"
+
+./setup_workflow_workdir.sh "$PWD" "$wd"
+
+1>&2 echo 'Running robomics/call_tad_cliques...'
+./run_external_workflow.sh \
+  "$wd" \
+  'workflows/robomics-call_tad_cliques-v0.4.2.tar.xz' \
+   configs/call_tad_cliques_merged_with_masking.json
+
+wd=".nextflow-robomics-call-tad-cliques-wd4"
+mkdir -p "$wd"
+
+./setup_workflow_workdir.sh "$PWD" "$wd"
+
+1>&2 echo 'Running robomics/call_tad_cliques...'
+./run_external_workflow.sh \
+  "$wd" \
+  'workflows/robomics-call_tad_cliques-v0.4.2.tar.xz' \
+   configs/call_tad_cliques_repl_with_masking.json
+
+
 step='postprocess_call_tad_cliques'
 wd=".nextflow-$step-wd"
 mkdir -p "$wd"
